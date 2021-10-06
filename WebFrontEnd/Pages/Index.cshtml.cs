2d3db2af57db13d1efc31037936c57726ab82a46
S. M. JAHANGIR <smjxpro@gmail.com>
2021-10-06 22:49:04 +0600

    added docker-compose files

---
+++ WebFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebFrontEnd.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnGet()
+    {
+        ViewData["Message"] = "Hello from webFrontEnd";
+
+        using (var client=new HttpClient())
+        {
+            var request =new HttpRequestMessage();
+            request.RequestUri = new Uri("http://mywebapi/WeatherForecast");
+            var response = client.Send(request);
+            ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+        }
+    }
+}
